ae9d679a89cc2cb543efa4b2a244f17427a0014b
Magno Rodríguez <magno.rodriguez.cubillos@gmail.com>
2024-10-23 03:21:18 -0300

    Funcionalidad de agregar/modificar productos, con lista de productos disponibles y mensajes de éxito para agregar, modificar y eliminar.

--- core/forms.py
+++ core/forms.py
@@ -1,28 +1,5 @@
 from django import forms
-from .models import Producto, Usuario
-
-
-
-
-
-from django.contrib.auth.forms import UserCreationForm
-
-
-class RegistroForm(UserCreationForm):
-    class Meta:
-        model = Usuario
-        fields = ['nombre', 'correo', 'pregunta_recuperacion', 'respuesta_pregunta', 'password1', 'password2', 'rol']
-        widgets = {
-            'nombre': forms.TextInput(attrs={'class': 'form-control'}),
-            'correo': forms.EmailInput(attrs={'class': 'form-control'}),
-            'respuesta_pregunta': forms.TextInput(attrs={'class': 'form-control'}),
-            'rol': forms.Select(attrs={'class': 'form-control'}),
-        }
-
-
-
-
-
+from .models import Producto
 
 class ProductoForm(forms.ModelForm):
     class Meta:
--- core/urls.py
+++ core/urls.py
@@ -3,14 +3,19 @@ from . import views
 from django.conf.urls.static import static
 from django.conf import settings
 from .views import generar_pdf_pedido
+
 urlpatterns = [
-    path('', views.lista_productos, name='lista_productos'),  # Listar productos
-    path('agregar/<int:producto_id>/', views.agregar_al_carrito, name='agregar_al_carrito'),  # Añadir producto al carrito
-    path('carrito/', views.ver_carrito, name='ver_carrito'),  # Ver carrito
-    path('eliminar/<int:producto_id>/', views.eliminar_del_carrito, name='eliminar_del_carrito'),  # Eliminar del carrito
-    path('checkout/', views.checkout, name='checkout'),  # Proceso de checkout
+    path('', views.lista_productos, name='lista_productos'),
+    path('agregar/<int:producto_id>/', views.agregar_al_carrito, name='agregar_al_carrito'),
+    path('carrito/', views.ver_carrito, name='ver_carrito'),
+    path('eliminar/<int:producto_id>/', views.eliminar_del_carrito, name='eliminar_del_carrito'),
+    path('checkout/', views.checkout, name='checkout'),
     path('pedido/<int:pedido_id>/pdf/', generar_pdf_pedido, name='generar_pdf_pedido'),
+    path('agregar_producto/', views.agregar_modificar_producto, name='agregar_producto'),
+    path('modificar_producto/<int:producto_id>/', views.agregar_modificar_producto, name='modificar_producto'),
+    path('borrar_producto/<int:producto_id>/', views.borrar_producto, name='borrar_producto'),
 ]
+
 
 
 if settings.DEBUG:
