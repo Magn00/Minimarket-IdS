5698890d84ba9c1bb1483d5d59e425b1ecaea7e4
Magno Rodríguez <magno.rodriguez.cubillos@gmail.com>
2024-10-23 03:21:18 -0300

    Funcionalidad de agregar/modificar productos, con lista de productos disponibles y mensajes de éxito para agregar, modificar y eliminar.

--- proyectoMinimarket/settings.py
+++ proyectoMinimarket/settings.py
@@ -12,6 +12,15 @@ https://docs.djangoproject.com/en/5.0/ref/settings/
 
 from pathlib import Path
 import os
+from django.contrib.messages import constants as messages
+
+MESSAGE_TAGS = {
+    messages.DEBUG: 'debug',
+    messages.INFO: 'info',
+    messages.SUCCESS: 'success',
+    messages.WARNING: 'warning',
+    messages.ERROR: 'danger',
+}
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
